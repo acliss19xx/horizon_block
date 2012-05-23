4350a271c5107fa580fd67a3c83f0c087de03a5e
acliss19xx <acliss19xx@gmail.com>
2012-05-23 12:00:55 +0900

    Update kansaiug_horizon/auto.js

--- kansaiug_horizon/auto.js
+++ kansaiug_horizon/auto.js
@@ -1,21 +1,19 @@
 ccmValidateBlockForm = function() {
 	
-	if ($('select[name=field_1_select_value]').val() == '' || $('select[name=field_1_select_value]').val() == 0) {
-		ccm_addError('要求された選択肢がありません: 横幅');
+	if ($('select[name=width]').val() == '' || $('select[name=width]').val() == 0) {
+		ccm_addError('要求された選択肢がありません: 長さ');
 	}
 
-	if ($('select[name=field_2_select_value]').val() == '' || $('select[name=field_2_select_value]').val() == 0) {
-		ccm_addError('要求された選択肢がありません: サイズ');
+	if ($('select[name=size]').val() == '' || $('select[name=size]').val() == 0) {
+		ccm_addError('要求された選択肢がありません: 太さ');
 	}
 
-	if ($('select[name=field_3_select_value]').val() == '' || $('select[name=field_3_select_value]').val() == 0) {
-		ccm_addError('要求された選択肢がありません: 配置');
-	}
-
-	if ($('select[name=field_4_select_value]').val() == '' || $('select[name=field_4_select_value]').val() == 0) {
+	if ($('select[name=color]').val() == '' || $('select[name=color]').val() == 0) {
 		ccm_addError('要求された選択肢がありません: 色');
 	}
 
-
+	if ($('select[name=height]').val() == '' || $('select[name=height]').val() == 0) {
+		ccm_addError('要求された選択肢がありません: 高さ');
+	}
 	return false;
 }
