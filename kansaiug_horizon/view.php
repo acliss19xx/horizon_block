33ccb0327ad289a22f1eca66648314617933259b
acliss19xx <acliss19xx@localhost>
2012-06-03 23:37:33 +0900

    ラインが中央に来るように変更

--- kansaiug_horizon/view.php
+++ kansaiug_horizon/view.php
@@ -1,11 +1,12 @@
 <?php  defined('C5_EXECUTE') or die("Access Denied."); ?>
 
-<div id="line-<?php echo $bID; ?>" style="<?php
-	echo 'border-bottom:' . $size . 'px ' . $borderstyle . ' '. $color . ';';
-	echo ' width:' . $width . '%;';
-	echo ' height:' . $height . 'px;';
-	echo '"';
-	echo '>';
-?>
-
+<div id="line-<?php echo $bID; ?>outer1" style="display:table; height: <?php echo $height; ?>px;width: 100%">
+	<div ="line-<?php echo $bID; ?>outer2" style="display : table-cell;vertical-align: middle;width: 100%"> 
+		<div id="line-<?php echo $bID; ?>inner" style="<?php
+			echo 'border-bottom:' . $size . 'px ' . $borderstyle . ' '. $color . ';';
+			echo ' width:' . $width . '%;';
+			echo '">';
+		?>
+		</div>
+	</div>
 </div>
