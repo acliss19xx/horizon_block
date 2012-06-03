ffa9e83dbd7c57f42aa90e8a9a70c5b35df7473d
acliss19xx <acliss19xx@gmail.com>
2012-06-04 07:33:31 +0900

    Update master

--- kansaiug_horizon/view.php
+++ kansaiug_horizon/view.php
@@ -2,7 +2,7 @@
 
 <div id="line-<?php echo $bID; ?>outer1" style="display:table; height: <?php echo $height; ?>px;width: 100%">
 	<div ="line-<?php echo $bID; ?>outer2" style="display : table-cell;vertical-align: middle;width: 100%"> 
-		<div id="line-<?php echo $bID; ?>inner style="<?php
+		<div id="line-<?php echo $bID; ?>inner" style="<?php
 			switch($align)
 			{
 				case 'left'  : echo "margin-right:auto;";
