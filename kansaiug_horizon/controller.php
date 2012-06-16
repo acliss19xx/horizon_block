e5f1c743084f62d76016b140408bdc1a8d293ad4
acliss19xx <acliss19xx@gmail.com>
2012-06-16 18:01:20 +0900

    Update master

--- kansaiug_horizon/controller.php
+++ kansaiug_horizon/controller.php
@@ -15,7 +15,6 @@ class KansaiugHorizonBlockController extends BlockController {
 	protected $btCacheBlockOutputForRegisteredUsers = false;
 	protected $btCacheBlockOutputLifetime = CACHE_LIFETIME;
 	
-	public function getSearchableContent() {
-		return $this->content;
+	public function view() {
 	}
 }
